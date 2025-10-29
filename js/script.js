6049d895614c5803e7ca26fe710a01cfa4a36dec
jbAfpa <jea.vau@dwwm.afpa>
2025-10-29 11:13:28 +0100

    js script casi fini bug affichage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 "use strict"
 
-import { Animal } from "./Animal.class.js";
+    import { Animal } from "./Animal.class.js";
+
+    // déclarer le variable 
+    let inputNom = document.getElementById("idNom");
+    let inputEspece = document.getElementById("idEsp");
+    let inputVol = document.getElementById("idVol");
+    let btnCreer = document.getElementById("btnCreer");
+    let btnVider = document.getElementById("btnvide");
+    let divErr = document.getElementById("idErr");
+    let divListe = document.getElementById("idListe");
+
+    // abonner les btn
+
+    btnCreer.addEventListener("click", recupChamps);
+    // btnVider.addEventListener("click", viderListe);
+
+    function recupChamps(event) {
+        // prend les valeurs du form
+        let nom = inputNom.value;
+        let espece = inputEspece.value;
+        let vol = inputVol.checked;
+
+        try {
+            // et creer un animal avec les valeur
+            let animal = new Animal (nom,espece,vol);
+            //afficher en ajoutant le afficher() dans la liste
+            divListe.innerHTML += animal.afficher() + "\n+<br>\n";
+        } catch(err) {
+            divErr.innerText = err.message;
+        }
+    
+    }
